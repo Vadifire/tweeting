2f7070d9ba60bfe92c6c56c80f9a5caf895dd7df
Cedric Debelle <cedric.debelle@khoros.com>
2019-04-24 10:56:05 -0500

    Created Post class to hold a Status Update's text

--- src/ProgramOne.java
+++ src/ProgramOne.java
@@ -6,21 +6,42 @@ import twitter4j.TwitterFactory;
 public class ProgramOne {
 
     public static void main (String[] args){
-        System.out.println(updateStatus("Update Status Test"));
+        System.out.println(updateStatus(new Post("post test")));
     }
 
 
     /*
-     * Returns false iff Twitter service or network is unavailable.
+     * Returns false iff Twitter service or network is unavailable, returns true otherwise.
      */
-    public static boolean updateStatus(String statusText){
+    public static boolean updateStatus(Post post){
+
         Twitter twitter = TwitterFactory.getSingleton();
         try {
-            Status status = twitter.updateStatus(statusText);
+            Status status = twitter.updateStatus(post.getText());
         } catch (TwitterException e){
             e.printStackTrace();
             return false;
         }
         return true;
+    }
+}
+
+/*
+ * Represents a post to be published on Twitter (text body only)
+ *
+ * Full Status objects are implemented by StatusJSONImpl.java in the Twitter4j API,
+ * but this Post class supports simply specifying a String as the post body
+ */
+class Post {
+
+    private String text;
+
+
+    public Post(String text) {
+        this.text = text;
+    }
+
+    public String getText(){
+        return text;
     }
 }

a8aac1bdacab5a27316224d7cdbfc30028aab062
Cedric Debelle <cedric.debelle@khoros.com>
2019-04-24 10:57:37 -0500

    Moved Post to a seperate file

---
+++ src/Post.java
@@ -0,0 +1,19 @@
+/*
+ * Represents a post to be published on Twitter (text body only)
+ *
+ * Full Status objects are implemented by StatusJSONImpl.java in the Twitter4j API,
+ * but this Post class supports simply specifying a String as the post body
+ */
+
+public class Post {
+    private String text;
+
+
+    public Post(String text) {
+        this.text = text;
+    }
+
+    public String getText(){
+        return text;
+    }
+}
--- src/ProgramOne.java
+++ src/ProgramOne.java
@@ -25,23 +25,3 @@ public class ProgramOne {
         return true;
     }
 }
-
-/*
- * Represents a post to be published on Twitter (text body only)
- *
- * Full Status objects are implemented by StatusJSONImpl.java in the Twitter4j API,
- * but this Post class supports simply specifying a String as the post body
- */
-class Post {
-
-    private String text;
-
-
-    public Post(String text) {
-        this.text = text;
-    }
-
-    public String getText(){
-        return text;
-    }
-}

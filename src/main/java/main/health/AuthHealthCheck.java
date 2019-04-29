1edbe2e3fe3f645d0099339e3c1e26615441e6e8
Cedric Debelle <cedric.debelle@khoros.com>
2019-04-29 16:56:02 -0500

    Improved credentials check

--- src/main/java/main/health/AuthHealthCheck.java
+++ src/main/java/main/health/AuthHealthCheck.java
@@ -4,7 +4,8 @@ import com.codahale.metrics.health.HealthCheck;
 import twitter4j.Twitter;
 import twitter4j.TwitterFactory;
 import twitter4j.auth.Authorization;
-import twitter4j.auth.NullAuthorization;
+import twitter4j.TwitterException;
+import twitter4j.User;
 
 /*
  * This Health check reports 'unhealthy' if Twitter authentication is missing (null) and healthy otherwise.
@@ -24,7 +25,14 @@ public class AuthHealthCheck extends HealthCheck {
             return Result.unhealthy("Twitter authentication credentials are not set. " +
                     "See http://twitter4j.org/en/configuration.html for help setting up authentication.");
         }
-        return Result.healthy();
+
+        try {
+            User user = twitter.verifyCredentials();
+            return Result.healthy("Credentials were verified.");
+
+        } catch (TwitterException e){
+            return Result.unhealthy("Twitter authentication failed: " + e.getErrorMessage());
+        }
     }
 
 }

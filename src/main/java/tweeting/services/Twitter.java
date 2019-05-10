53a6df3ff4dd50ecea44203d047d2aeb8aa5c86d
Cedric Debelle <cedric.debelle@khoros.com>
2019-05-10 15:29:47 -0500

    Change to lazy init

--- src/main/java/tweeting/services/Twitter.java
+++ src/main/java/tweeting/services/Twitter.java
@@ -4,11 +4,15 @@ package tweeting.services;
  * Contains Twitter4J's functionality
  */
 public class Twitter {
-    private static final Twitter instance = new Twitter();
+    
+    private static Twitter instance;
 
     private Twitter(){}
 
     public static Twitter getInstance() {
+        if (instance == null) {
+            return new Twitter();
+        }
         return instance;
     }
 }
